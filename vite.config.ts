670dd8a7051cf1de202dea80b1cf4a6c5f02659f
cecile.valente <cecile.valente@docaposte.fr>
2023-08-24 10:31:26 +0200

    prettier and eslint init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/* eslint-disable import/no-extraneous-dependencies */
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    open: true,
+    hmr: { overlay: false },
+  },
+  plugins: [
+    react(),
+    checker({
+      terminal: false,
+      enableBuild: false,
+      eslint: {
+        lintCommand: "eslint './src/**/*.{ts,tsx}'",
+      },
+    }),
+  ],
+});
