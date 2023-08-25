a61376726cac7473d991ebebc01d7e3af7db9d8a
cecile.valente <cecile.valente@docaposte.fr>
2023-08-25 17:59:20 +0200

    feat-joueur: wip to back up

--- src/board/joueur/Joueur.tsx
+++ src/board/joueur/Joueur.tsx
@@ -1,64 +1,86 @@
 import { Grid, Typography } from "@mui/material";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import GameContext from "../../providers/GameContext";
-import { getHandValue, getRandomCard } from "../../utils/player/HandsValue";
+import { getHandValue, getRandomCard } from "../../utils/player/JoueurUtils";
+import { Cards } from "../../utils/types/cardsDeck.types";
+import AceModal from "./AceModal";
+import GenerateCard from "./GenerateCard";
 
 const Joueur = () => {
   const { playerCards, setPlayerCards, cardsDeck } = useContext(GameContext);
-
+  const [pickedCards, setPickedCards] = useState<Cards[]>();
+  const [openAceModal, setOpenAceModal] = useState(false);
   const currentDeck = cardsDeck;
-  const playerPickCard = () => {
-    console.log(cardsDeck);
-    const firstCard = getRandomCard(currentDeck);
-    console.log(firstCard);
-    const secoundCard = getRandomCard(currentDeck);
-    console.log(secoundCard);
-    console.log(cardsDeck);
-    const newCards = [...playerCards, firstCard, secoundCard];
-    console.log(newCards);
-    setPlayerCards(newCards);
+
+  const handleCloseAceModal = () => {
+    setOpenAceModal(false);
+  };
+
+  const firstPlayerHand = () => {
+    if (playerCards.length === 0) {
+      const firstCard = getRandomCard(currentDeck);
+      const secoundCard = {
+        card: "As de trèfle",
+        value: 0,
+        img: "/images/cards/ace_of_clubs.svg",
+      };
+
+      const newCards = [...playerCards, firstCard, secoundCard];
+
+      setPickedCards(newCards);
+    }
   };
 
   useEffect(() => {
-    playerPickCard();
+    firstPlayerHand();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (pickedCards) {
+      if (pickedCards.some((card) => card.value === 0)) {
+        if (pickedCards.findIndex((card) => card.value === 0) !== 0) {
+          setPlayerCards(pickedCards.filter((card) => card.value !== 0));
+        }
+        setOpenAceModal(true);
+      } else {
+        setPlayerCards(pickedCards);
+      }
+    }
+  }, [pickedCards]);
+
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="space-around"
-    >
-      <Grid item>
-        <Typography variant="h4">Joueur</Typography>
-      </Grid>
-      <Grid item>
-        <Grid container alignItems="center">
-          {playerCards.map((card) => (
-            <Grid
-              item
-              sx={{ textAlign: "center" }}
-              key={`player-card-${card.card}`}
-            >
-              <Typography variant="body1">{card.card}</Typography>
-              <img
-                className="cards"
-                src="/images/cards/2_of_clubs.svg"
-                alt="Paquet de carte"
-              />
-            </Grid>
-          ))}
+    <>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Grid item>
+          <Typography variant="h4">Joueur</Typography>
+        </Grid>
+        <Grid item>
+          <Grid container alignItems="center">
+            {playerCards.length >= 1 && <GenerateCard />}
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Typography variant="h6">
+            {`Valeur de la main : ${getHandValue(playerCards)}`}
+          </Typography>
         </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="h6">
-          {`Valeur de la main : ${getHandValue(playerCards)}`}
-        </Typography>
-      </Grid>
-    </Grid>
+      {pickedCards && (
+        <AceModal
+          openModal={openAceModal}
+          onCloseModal={handleCloseAceModal}
+          cards={pickedCards}
+          setCards={setPickedCards}
+        />
+      )}
+    </>
   );
 };
 
